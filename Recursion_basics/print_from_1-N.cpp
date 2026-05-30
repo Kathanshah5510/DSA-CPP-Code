b7ea6422daa2652eeafee680e3a2e9ef080fd3af
Kathanshah5510 <kathanshah5511@gmail.com>
2026-05-30 12:01:00 +0530

    written code to print 1 to N recursively

---
+++ Recursion_basics/print_from_1-N.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void rec(int num){
+    while(num > 0){
+        rec(num-1);
+        cout << num << endl;
+        break;
+    }
+}
+
+int main(){
+    int n = 10;
+    // cout << "Enter till which number you want to print: ";
+    // cin >> n;
+    rec(n);
+
+    return 0;
+}
