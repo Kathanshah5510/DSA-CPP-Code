cdceb715f0e96a384bfbb3142de5c45ef395a4c3
Kathanshah5510 <kathanshah5511@gmail.com>
2026-06-09 09:10:27 +0530

    modified the file for printing 1 to N to use recursion function using backtracking

--- Recursion_basics/print_from_1-N.cpp
+++ Recursion_basics/print_from_1-N.cpp
@@ -1,19 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void rec(int num){
-    while(num > 0){
-        rec(num-1);
-        cout << num << endl;
-        break;
+// void rec(int num){
+//     while(num > 0){
+//         rec(num-1);
+//         cout << num << endl;
+//         break;
+//     }
+// }
+
+void rec(int i, int num){
+    if(i < 1){
+        return;
     }
+    rec(i-1, num);
+    cout << i << endl;
 }
 
 int main(){
     int n = 10;
     // cout << "Enter till which number you want to print: ";
     // cin >> n;
-    rec(n);
+    // rec(n);
+    rec(n, n);
 
     return 0;
 }
