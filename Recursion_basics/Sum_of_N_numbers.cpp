f1b7c00130adc1a55f1f38e805af76a95c455d2a
Kathanshah5510 <kathanshah5511@gmail.com>
2026-06-22 12:56:35 +0530

    refactored summation function to use a return value instead of printing directly (use functinal way instead of using parameterized)

--- Recursion_basics/Sum_of_N_numbers.cpp
+++ Recursion_basics/Sum_of_N_numbers.cpp
@@ -1,21 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// parameterized
-void rec(int n, int sum){
-    if(n < 1){
-        cout << "The result if sumation is: " << sum;
+// // parameterized
+// void rec(int n, int sum){
+//     if(n < 1){
+//         cout << "The result if sumation is: " << sum;
+//     }
+//     else{
+//         rec(n - 1, sum + n);    
+//     }
+// }
+
+// functional
+int rec(int n){
+    if(n == 0){
+        return 0;
     }
-    else{
-        rec(n - 1, sum + n);    
-    }
+    else return n + rec(n - 1);
 }
 
 int main(){
     int n;
     cout << "Enter the integer to which you want the sum to be: ";
     cin >> n;
-    rec(n, 0);
+    // rec(n, 0);
+    int res = rec(n);
+    cout << "The result of summation is: " << res << endl;
     
     return 0;
 }
