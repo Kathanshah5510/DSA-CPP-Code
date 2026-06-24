b49376233bac1cd8544ef5b76005d2ca02ee3af7
Kathanshah5510 <kathanshah5511@gmail.com>
2026-06-24 10:11:55 +0530

    refactored reverse function to use a single index for swapping elements

--- Recursion_basics/Reverse_array.cpp
+++ Recursion_basics/Reverse_array.cpp
@@ -1,21 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void reverse(vector<int> &arr, int left, int right){
-    if(left >= right){
-        return;
-    }
-    swap(arr[left], arr[right]);
-    reverse(arr, left + 1, right - 1);
-}
-
-// void reverse(vector<int> &arr, int i){
-//     if(i >= arr.size() / 2){
+// void reverse(vector<int> &arr, int left, int right){
+//     if(left >= right){
 //         return;
 //     }
-//     swap(arr[i], arr[arr.size() - i - 1]);
-//     reverse(arr, i + 1);
+//     swap(arr[left], arr[right]);
+//     reverse(arr, left + 1, right - 1);
 // }
+
+void reverse(vector<int> &arr, int i){
+    if(i >= arr.size() / 2){
+        return;
+    }
+    swap(arr[i], arr[arr.size() - i - 1]);
+    reverse(arr, i + 1);
+}
 
 int main(){
     int n;
@@ -26,8 +26,8 @@ int main(){
     for(int i = 0; i < n; i++){
         cin >> arr[i];
     }
-    reverse(arr, 0, n - 1);
-    // reverse(arr, 0);
+    // reverse(arr, 0, n - 1);
+    reverse(arr, 0);
     cout << "The reversed array is: ";
     for(int i = 0; i < n; i++){
         cout << arr[i] << " ";
