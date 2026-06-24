7f4933f082638040d52e99d11592fd8f47891203
Kathanshah5510 <kathanshah5511@gmail.com>
2026-06-24 21:32:13 +0530

    added implementation of preorder traversal for binary tree

---
+++ Tree/Preorder_traversal.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) : data(val), left(nullptr), right(nullptr) {}
+};
+
+void preorder(Node* root){
+    if(root == nullptr) return;
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+int main(){
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(7);
+    root->right->right = new Node(8);
+    root->left->right->left = new Node(6);
+    root->right->right->left = new Node(9);
+    root->right->right->right = new Node(10);
+
+    //         1
+    //       /   \
+    //      2     3
+    //     / \   / \
+    //    4   5 7   8
+    //       /     / \
+    //      6     9   10
+    
+    cout << "Preorder Traversal: ";
+    preorder(root);
+    cout << endl;
+
+    return 0;
+}
