b57e9e64fd11b43339508af1568bc793c25d8267
Kathanshah5510 <kathanshah5511@gmail.com>
2026-07-21 22:42:23 +0530

    added implementation of minimizing coins to achieve target amount using dynamic programming

---
+++ CSES-DP/Minimizing_coins.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n, x;
+    cout << "Enter the number of coins and the target amount: ";
+    cin >> n >> x;
+
+    vector<int> coins(n);
+    cout << "Enter the coin denominations: ";
+    for(int i = 0; i < n; i++){
+        cin >> coins[i];
+    }
+
+    const int INF = 1e9;
+    vector<int> dp(x + 1, INF);
+    dp[0] = 0;
+
+    for(int i = 1; i <= x; i++){
+        for(int j : coins){
+            if(i - j >= 0){
+                dp[i] = min(dp[i], dp[i - j] + 1);
+            }
+        }
+    }
+    cout << "Minimum number of coins needed: ";
+    if(dp[x] == INF){
+        cout << -1 << endl;
+    } else {
+        cout << dp[x] << endl;
+    }
+
+    return 0;
+}
