de8039805c0015359d6f55dd44a9a6a02ee6439e
Kathanshah5510 <kathanshah5511@gmail.com>
2026-07-25 23:28:36 +0530

    refactor: rename variable for clarity and update logic to use target amount

--- CSES-DP/Minimizing_coins.cpp
+++ CSES-DP/Minimizing_coins.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 int main(){
-    int n, x;
+    int n, target;
     cout << "Enter the number of coins and the target amount: ";
-    cin >> n >> x;
+    cin >> n >> target;
 
     vector<int> coins(n);
     cout << "Enter the coin values: ";
@@ -13,21 +13,21 @@ int main(){
     }
 
     const int INF = 1e9;
-    vector<int> dp(x + 1, INF);
+    vector<int> dp(target + 1, INF);
     dp[0] = 0;
 
-    for(int i = 1; i <= x; i++){
-        for(int j : coins){
-            if(i - j >= 0  && dp[i - j] != INF){
-                dp[i] = min(dp[i], dp[i - j] + 1);
+    for(int i = 1; i <= target; i++){
+        for(int coin : coins){
+            if(i - coin >= 0  && dp[i - coin] != INF){
+                dp[i] = min(dp[i], dp[i - coin] + 1);
             }
         }
     }
     cout << "Minimum number of coins needed: ";
-    if(dp[x] == INF){
+    if(dp[target] == INF){
         cout << -1 << endl;
     } else {
-        cout << dp[x] << endl;
+        cout << dp[target] << endl;
     }
 
     return 0;
