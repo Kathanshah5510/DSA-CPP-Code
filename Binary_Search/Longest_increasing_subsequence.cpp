47057f40ef92c61bc10117b9de47afeb1628562e
Kathanshah5510 <kathanshah5511@gmail.com>
2026-07-26 21:53:59 +0530

    modified tails vector with temp for longest increasing subsequence calculation

--- Binary_Search/Longest_increasing_subsequence.cpp
+++ Binary_Search/Longest_increasing_subsequence.cpp
@@ -11,16 +11,29 @@ int main(){
         cin >> arr[i];
     }
 
-    vector<int> tails;
-    for(int num : arr){
-        int pos = lower_bound(tails.begin(), tails.end(), num) - tails.begin();
-        if(pos == tails.size()){
-            tails.push_back(num);
-        } else {
-            tails[pos] = num;
+    // vector<int> tails;
+    // for(int num : arr){
+    //     int pos = lower_bound(tails.begin(), tails.end(), num) - tails.begin();
+    //     if(pos == tails.size()){
+    //         tails.push_back(num);
+    //     } else {
+    //         tails[pos] = num;
+    //     }
+    // }
+    // cout << "The length of the longest increasing subsequence is: " << tails.size() << endl;
+
+    vector<int> temp;
+    temp.push_back(arr[0]);
+    for(int i = 0; i < arr.size(); i++){
+        if(arr[i] > temp.back()){
+            temp.push_back(arr[i]);
+        }
+        else{
+            int ind = lower_bound(temp.begin(), temp.end(), arr[i]) - temp.begin();
+            temp[ind] = arr[i];
         }
     }
-    cout << "The length of the longest increasing subsequence is: " << tails.size() << endl;
+    cout << "The length of the longest increasing subsequence is: " << temp.size() << endl;
 
     return 0;
 }
